3087319e3b42e6b61551743ecc8d5f1750a7758e
( Odi ) Thanapat Sombatkamrai <30354428+MSMasterX@users.noreply.github.com>
2022-06-07 13:03:58 +0200

    3 and 6

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("input first number: ");
+        int number1 = scanner.nextInt();
+        System.out.println("input second number: ");
+        int number2 = scanner.nextInt();
+        int sum = number1 + number2;
+        int minus = number1 - number2;
+        int multiply = number1 * number2;
+        int divide = number1 / number2;
+        System.out.printf("Sum = %d\nMinus = %d\nMultiply  = %d\nDivide = %d\n ", sum, minus, multiply, divide);
+    }
+}
